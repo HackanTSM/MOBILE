af66bbc826d17615c590c859df0355e55c04c2e4
Petrault Matteo <124884198+pitchounnnee@users.noreply.github.com>
2024-01-12 09:06:03 +0100

    api hackathon
    ok

--- mobile/src/app/atelier/atelier.page.ts
+++ mobile/src/app/atelier/atelier.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-atelier',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AtelierPage implements OnInit {
 
-  constructor() { }
+  event : any={};
+
+  constructor( private router: Router, private activatedRoute: ActivatedRoute)  { 
+    this.activatedRoute.queryParams.subscribe(params =>{
+      let navigation:any = this.router.getCurrentNavigation()?.extras.state;
+      this.event = navigation.param1;
+
+    })
+  }
 
   ngOnInit() {
   }
--- mobile/src/app/hackathon/hackathon.page.ts
+++ mobile/src/app/hackathon/hackathon.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -9,23 +10,47 @@ import { NavController } from '@ionic/angular';
 export class HackathonPage implements OnInit {
 
   isFavori: boolean[] = [];
+  HackathonFiltres: any = [];
 
-  constructor(private navCtrl: NavController) {}
+  constructor(private navCtrl: NavController, private router:Router) {
 
-  events = [
+    const apiUrl = `http://192.168.66.20:3000/hackathon`; 
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log('API Response:', data);
+        this.events = data;
+        if (data && Array.isArray(data)) {
+          this.HackathonFiltres = data;
+        } else {
+          console.error('Erreur', data);
+        }
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des données API', error);
+      });
+  }
+  events:any = [];
+  /*events = [
     { title: "Description de l'événement1", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg" },
     { title: "Description de l'événement2", imageUrl: "https://minutebag.com/wp-content/uploads/2019/03/images.jpg"},
     { title: "Description de l'événement3", imageUrl: "https://previews.123rf.com/images/kchung/kchung1610/kchung161001354/64508202-test-%C3%A9crit-%C3%A0-la-main-%C3%A9crit-%C3%A0-la-main-%C3%A0-bord-transparent-photo.jpg" }
-  ];
+  ];*/
 
   ngOnInit() {
     this.isFavori = new Array(this.events.length).fill(false);
   }
 
-  voirAtelier() {
-    this.navCtrl.navigateForward('/atelier');
+  voirAtelier(evnt:any) {
+
+    let navigationExtras: NavigationExtras = {
+      state : {
+        param1: evnt
+      }
+    };
+    this.router.navigate(['/atelier'],navigationExtras);
   }
-  // Fonction pour basculer l'état du favori
+
   toggleFavori(event: Event, index: number) {
     event.stopPropagation(); // Empêche la propagation du clic vers le parent
     this.isFavori[index] = !this.isFavori[index];
@@ -36,8 +61,9 @@ export class HackathonPage implements OnInit {
     this.navCtrl.navigateForward('/favoris', { state: { favoris } });
   }
 
-  // Fonction pour obtenir la liste des hackathons favoris
   getFavoris() {
-    return this.events.filter((event, index) => this.isFavori[index]);
+    //return this.events.filter((event, index) => this.isFavori[index]);
   }
+
+
 }
